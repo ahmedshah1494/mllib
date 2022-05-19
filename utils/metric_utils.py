2dcebf873e8dde3907bf0395163029ed6a2d8526
ahmed <ahmedshah1494@gmail.com>
2022-05-18 21:49:23 -0400

    modularized compute_accuracy

--- utils/metric_utils.py
+++ utils/metric_utils.py
@@ -1,11 +1,14 @@
 import torch
 
-
-def compute_accuracy(logits, y):
-    if (logits.dim() == 1) or ((logits.dim() == 2) and (logit.shape[1] == 1)):
+def get_preds_from_logits(logits):
+    if (logits.dim() == 1) or ((logits.dim() == 2) and (logits.shape[1] == 1)):
         pred = (logits > 0.5).float()
     else:
         pred = torch.argmax(logits, dim=1)
+    return pred
+
+def compute_accuracy(logits, y):
+    pred = get_preds_from_logits(logits)
     correct = (pred == y)
     acc = correct.sum().float() / len(y)
     return float(acc), correct
