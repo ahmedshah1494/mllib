efd5604f97c1404182310262838b56816d48d60c
ahmed <ahmedshah1494@gmail.com>
2022-05-27 11:26:27 -0400

    added apgd, square and hopskipjump attacks

--- adversarial/attacks.py
+++ adversarial/attacks.py
@@ -1,19 +1,26 @@
 from enum import Enum, auto
-from typing import Type
+from time import time
+from typing import List, Literal, Type, Union
 
 from attrs import define
 import attrs
 from mllib.param import BaseParameters
+import torch
 
 from torchattacks.attack import Attack
-from torchattacks import PGD
+import torchattacks
+import foolbox
 
 class SupportedAttacks(Enum):
     PGDL2 = auto()
     PGDLINF = auto()
+    APGDLINF = auto()
+    SQUARELINF = auto()
+    HOPSKIPJUMPLINF = auto()
 
 class SupportedBackend(Enum):
     TORCHATTACKS = auto()
+    FOOLBOX = auto()
 
 @define(slots=False)
 class AbstractAttackConfig:
@@ -24,7 +31,7 @@ class AbstractAttackConfig:
 
 @define(slots=False)
 class TorchAttackPGDInfParams(AbstractAttackConfig):
-    _cls = PGD
+    _cls = torchattacks.PGD
     eps: float = 8/255
     nsteps: int = 10
     step_size: float = eps / (nsteps/2)
@@ -36,12 +43,70 @@ class TorchAttackPGDInfParams(AbstractAttackConfig):
         d['alpha'] = d.pop('step_size')
         return d
 
+@define(slots=False)
+class TorchAttackAPGDInfParams(AbstractAttackConfig):
+    _cls = torchattacks.APGD
+    eps: float = 8/255
+    nsteps: int = 10
+    seed: int = time()
+    n_restarts: int = 1
+
+    def asdict(self):
+        d = super().asdict()
+        d['steps'] = d.pop('nsteps')
+        return d
+
+@define(slots=False)
+class TorchAttackSquareInfParams(AbstractAttackConfig):
+    _cls = torchattacks.Square
+    eps: float = 8/255
+    n_queries: int = 1000
+    seed: int = time()
+    n_restarts: int = 1
+
+class FoolboxAttackWrapper:
+    atkcls: Type[foolbox.attacks.base.Attack] = None
+    def __init__(self, model, **kwargs) -> None:
+        self.model = foolbox.PyTorchModel(model, bounds=(0, 1))
+        print(kwargs)
+        self.run_kwargs = kwargs.pop('run_params')
+        self.attack = self.atkcls(**kwargs)
+    
+    def __call__(self, x, y):
+        return self.attack(self.model, x, y, **(self.run_kwargs))
+
+class FoolboxHopSkipJumpInfWrapper(FoolboxAttackWrapper):
+    atkcls = foolbox.attacks.HopSkipJumpAttack
+@define(slots=False)
+class FoolboxCommonRunParams(AbstractAttackConfig):
+    epsilons: List[int] = [8/255]
+
+@define(slots=False)
+class FoolboxHopSkipJumpInfInitParams(AbstractAttackConfig):
+    _cls = FoolboxHopSkipJumpInfWrapper
+    steps: int = 64
+    initial_gradient_eval_steps: int = 100
+    max_gradient_eval_steps: int = 10000
+    stepsize_search: Union[
+        Literal["geometric_progression"], Literal["grid_search"]
+    ] = "geometric_progression"
+    gamma: float = 1.0
+    tensorboard: Union[Literal[False], None, str] = False
+    constraint: Union[Literal["linf"], Literal["l2"]] = "l2"
+    run_params: AbstractAttackConfig = FoolboxCommonRunParams()
+
 class AttackParamFactory:
     torchattack_params = {
-        SupportedAttacks.PGDLINF: TorchAttackPGDInfParams
+        SupportedAttacks.PGDLINF: TorchAttackPGDInfParams,
+        SupportedAttacks.APGDLINF: TorchAttackAPGDInfParams,
+        SupportedAttacks.SQUARELINF: TorchAttackSquareInfParams
+    }
+    foolbox_params = {
+        SupportedAttacks.HOPSKIPJUMPLINF: FoolboxHopSkipJumpInfInitParams
     }
     backend_params = {
-        SupportedBackend.TORCHATTACKS: torchattack_params
+        SupportedBackend.TORCHATTACKS: torchattack_params,
+        SupportedBackend.FOOLBOX: foolbox_params
     }
 
     @classmethod
