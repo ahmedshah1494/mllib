611a11e1ae0a754629da14f80278a2c696208776
ahmed <ahmedshah1494@gmail.com>
2022-06-21 10:24:03 +0500

    deepcopying objects assigned to static attributes

--- param.py
+++ param.py
@@ -1,11 +1,18 @@
 from typing import Type
 from attrs import define
 import attrs
+from copy import deepcopy
 
 
 @define(slots=False)
 class BaseParameters:
     cls: Type
+
+    def __init__(self, *args, **kwargs):
+        for k,v in vars(self).items():
+            if isinstance(v, object):
+                self.__setattr__(k, deepcopy(v))
+        self.__attrs_init__(*args, **kwargs)
 
     def asdict(self):
         return attrs.asdict(self)
