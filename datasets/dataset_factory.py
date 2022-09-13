d0ff4633c4898a53c0f4f23e318d6d187f2387e3
ahmed <ahmedshah1494@gmail.com>
2022-09-13 09:17:27 -0400

    added imagenet subsets as datasets

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -12,6 +12,7 @@ from mllib.utils.image_dataset_utils import filter_dataset_by_target, make_val_d
 from mllib.datasets.tiny_imagenet_dataset import TinyImagenetNPZDataset
 
 from mllib.datasets.imagenet_filelist_dataset import ImagenetFileListDataset, get_imagenet_webdataset
+from webdataset import WebDataset
 
 class AutoName(Enum):
     def _generate_next_value_(name, start, count, last_values):
@@ -22,6 +23,10 @@ class SupportedDatasets(AutoName):
     CIFAR100 = auto()
     TINY_IMAGENET = auto()
     IMAGENET = auto()
+    IMAGENET10 = auto()
+    IMAGENET100_64 = auto()
+    IMAGENET100_81 = auto()
+    IMAGENET100 = auto()
     MNIST = auto()
 
 class AbstractDatasetFactory(Parameterized):
@@ -64,10 +69,29 @@ class ImageDatasetFactory(AbstractDatasetFactory):
                                         TinyImagenetNPZDataset,
                                         200, 475, 5000
                                     ),
+        SupportedDatasets.IMAGENET10 : DatasetConfig(
+                                        TinyImagenetNPZDataset,
+                                        10, 1275, 250
+                                    ),
+        SupportedDatasets.IMAGENET100_64 : DatasetConfig(
+                                        TinyImagenetNPZDataset,
+                                        100, 1275, 2500
+                                    ),
+        SupportedDatasets.IMAGENET100_81 : DatasetConfig(
+                                        TinyImagenetNPZDataset,
+                                        100, 1275, 2500
+                                    ),
         SupportedDatasets.IMAGENET : DatasetConfig(
                                         get_imagenet_webdataset,
                                         1000, 127, 2
-                                        
+                                    ),
+        # SupportedDatasets.IMAGENET100 : DatasetConfig(
+        #                                 get_imagenet_webdataset,
+        #                                 100, 25, 1
+        #                             ),
+        SupportedDatasets.IMAGENET100 : DatasetConfig(
+                                        ImagenetFileListDataset,
+                                        100, 1275, 2500
                                     )
     }
 
@@ -111,17 +135,21 @@ class ImageDatasetFactory(AbstractDatasetFactory):
         if params.dataset in [SupportedDatasets.CIFAR10, SupportedDatasets.CIFAR100, SupportedDatasets.MNIST]:
             train_dataset = dataset_class('%s/'%params.datafolder, transform=train_transform, download=True)        
             test_dataset = dataset_class('%s/'%params.datafolder, train=False, transform=test_transform, download=True)
-        elif params.dataset == SupportedDatasets.TINY_IMAGENET:
+        elif cfg.dataset_class in [TinyImagenetNPZDataset, ImagenetFileListDataset]: #params.dataset in [SupportedDatasets.TINY_IMAGENET, SupportedDatasets.IMAGENET10, SupportedDatasets.IMAGENET100_64]:
             train_dataset = dataset_class(params.datafolder, transform=train_transform)
             test_dataset = dataset_class(params.datafolder, train=False, transform=test_transform)
-        elif params.dataset == SupportedDatasets.IMAGENET:
+        elif (cfg.dataset_class == WebDataset) or (params.dataset in [SupportedDatasets.IMAGENET]):#, SupportedDatasets.IMAGENET100]:
             num_train_shards = min(params.max_num_train, cfg.min_train_class_counts)
             num_val_shards = cfg.max_val_counts
             num_test_shards = params.max_num_test if params.max_num_test < np.inf else None
-            train_dataset = get_imagenet_webdataset(params.datafolder, nshards=num_train_shards, train=True, transform=train_transform)
-            val_dataset = get_imagenet_webdataset(params.datafolder, first_shard_idx=num_train_shards, nshards=num_val_shards, train=True, transform=train_transform)
-            test_dataset = get_imagenet_webdataset(params.datafolder, nshards=num_test_shards, train=False, transform=test_transform)
+            len_shard = 10_000 if params.dataset == SupportedDatasets.IMAGENET else 5_000
+            train_dataset = get_imagenet_webdataset(params.datafolder, nshards=num_train_shards, train=True, transform=train_transform, len_shard=len_shard)
+            val_dataset = get_imagenet_webdataset(params.datafolder, first_shard_idx=num_train_shards, nshards=num_val_shards, train=True, transform=train_transform, len_shard=len_shard)
+            test_dataset = get_imagenet_webdataset(params.datafolder, nshards=num_test_shards, train=False, transform=test_transform, len_shard=len_shard)
             return train_dataset, val_dataset, test_dataset, nclasses
+        elif cfg.dataset_class == torchvision.datasets.ImageFolder:
+            train_dataset = torchvision.datasets.ImageFolder(os.path.join(params.datafolder, 'train'), transform=train_transform)
+            test_dataset = torchvision.datasets.ImageFolder(os.path.join(params.datafolder, 'val'), transform=test_transform)
 
         filter_dataset_by_target(train_dataset, params.class_idxs)
         train_idxs, val_idxs = make_val_dataset(train_dataset, nclasses, train_class_counts, class_idxs=params.class_idxs)
