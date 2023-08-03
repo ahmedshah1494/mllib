8066d4c60c98037248a2e4977d64277db29176c3
ahmed <ahmedshah1494@gmail.com>
2023-08-02 23:56:25 -0700

    added sharded datasets for common corruptions

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -14,7 +14,7 @@ from mllib.datasets.fixation_point_dataset import FixationPointDataset
 
 from mllib.datasets.imagenet_filelist_dataset import ImagenetFileListDataset, get_webdataset, get_clickme_webdataset
 from webdataset import WebDataset
-from mllib.datasets.torchaudio_datasets import SpeechCommandDatasetWrapper
+from mllib.datasets.torchaudio_datasets import SpeechCommandDatasetWrapper, LibrispeechFilelistDataset
 
 class AutoName(Enum):
     def _generate_next_value_(name, start, count, last_values):
@@ -29,6 +29,7 @@ class SupportedDatasets(AutoName):
     ECOSET = auto()
     ECOSET_FOLDER = auto()
     ECOSETC_FOLDER = auto()
+    ECOSETC = auto()
     ECOSET10 = auto()
     ECOSET10_FOLDER = auto()
     ECOSET10wBB_FOLDER = auto()
@@ -39,6 +40,7 @@ class SupportedDatasets(AutoName):
     ECOSET100C_FOLDER = auto()
     ECOSET100 = auto()
     IMAGENET_FOLDER = auto()
+    IMAGENETC_FOLDER = auto()
     IMAGENET10 = auto()
     IMAGENET100_64 = auto()
     IMAGENET100_81 = auto()
@@ -48,6 +50,7 @@ class SupportedDatasets(AutoName):
     FMNIST = auto()
     SVHN = auto()
     SPEECHCOMMANDS = auto()
+    LIBRISPEECH = auto()
     CLICKME = auto()
 
 class AbstractDatasetFactory(Parameterized):
@@ -71,9 +74,14 @@ class ImageDatasetFactory(AbstractDatasetFactory):
         custom_transforms: list = None
         max_num_train: int = np.inf
         max_num_test: int = np.inf
+        kwargs: dict = {}
 
 
     dataset_config = {
+        SupportedDatasets.LIBRISPEECH: DatasetConfig(
+                                        LibrispeechFilelistDataset,
+                                        1000, 300_000, 300_000
+        ),
         SupportedDatasets.SPEECHCOMMANDS: DatasetConfig(
                                         SpeechCommandDatasetWrapper,
                                         10, 4000, 4000
@@ -138,6 +146,10 @@ class ImageDatasetFactory(AbstractDatasetFactory):
                                         ImagenetFileListDataset,
                                         565, 5000, 0
                                     ),
+        SupportedDatasets.ECOSETC : DatasetConfig(
+                                        get_webdataset,
+                                        565, 5000, 0
+                                    ),
         SupportedDatasets.ECOSET10 : DatasetConfig(
                                         TinyImagenetNPZDataset,
                                         10, 4800, 859
@@ -176,7 +188,11 @@ class ImageDatasetFactory(AbstractDatasetFactory):
                                     ),
         SupportedDatasets.IMAGENET_FOLDER : DatasetConfig(
                                         torchvision.datasets.ImageFolder,
-                                        1000, 1271, 10
+                                        1000, 5000, 5000
+                                    ),
+        SupportedDatasets.IMAGENETC_FOLDER : DatasetConfig(
+                                        ImagenetFileListDataset,
+                                        1000, 5000, 0
                                     ),
         # SupportedDatasets.IMAGENET100 : DatasetConfig(
         #                                 get_imagenet_webdataset,
@@ -223,9 +239,10 @@ class ImageDatasetFactory(AbstractDatasetFactory):
         max_val_counts = cfg.max_val_counts
         print(params, train_class_counts, max_val_counts, test_class_counts)
 
+        train_transform = test_transform = None
         if params.custom_transforms is not None:
             train_transform, test_transform = params.custom_transforms
-        else:
+        elif params.dataset != SupportedDatasets.LIBRISPEECH:
             train_transform, test_transform = cls.get_transforms(params.dataset)
         print(train_transform)
         print(test_transform)
@@ -239,12 +256,14 @@ class ImageDatasetFactory(AbstractDatasetFactory):
 
             setattr(train_dataset, 'targets', train_dataset.labels)
             setattr(test_dataset, 'targets', test_dataset.labels)
-        elif cfg.dataset_class in [TinyImagenetNPZDataset, ImagenetFileListDataset]: #params.dataset in [SupportedDatasets.TINY_IMAGENET, SupportedDatasets.IMAGENET10, SupportedDatasets.IMAGENET100_64]:
-            train_dataset = dataset_class(params.datafolder, split='train', transform=train_transform)
-            if os.path.exists(os.path.join(params.datafolder, 'val.pkl.npz')):
-                val_dataset = dataset_class(params.datafolder, split='val', transform=train_transform)
-            test_dataset = dataset_class(params.datafolder, split='test', transform=test_transform)
-        elif (cfg.dataset_class == WebDataset) or (params.dataset in [SupportedDatasets.IMAGENET, SupportedDatasets.ECOSET, SupportedDatasets.ECOSET100, SupportedDatasets.CLICKME]):#, SupportedDatasets.IMAGENET100]:
+        elif cfg.dataset_class in [TinyImagenetNPZDataset, ImagenetFileListDataset, LibrispeechFilelistDataset]: #params.dataset in [SupportedDatasets.TINY_IMAGENET, SupportedDatasets.IMAGENET10, SupportedDatasets.IMAGENET100_64]:
+            train_dataset = dataset_class(params.datafolder, split='train', transform=train_transform, **(params.kwargs))
+            if os.path.exists(os.path.join(params.datafolder, 'val.pkl.npz')) or os.path.exists(os.path.join(params.datafolder, 'val_paths.txt')):
+                val_dataset = dataset_class(params.datafolder, split='val', transform=train_transform, **(params.kwargs))
+            test_dataset = dataset_class(params.datafolder, split='test', transform=test_transform, **(params.kwargs))
+        elif (cfg.dataset_class == WebDataset) or (params.dataset in [SupportedDatasets.IMAGENET, SupportedDatasets.ECOSET,
+                                                                      SupportedDatasets.IMAGENET, SupportedDatasets.ECOSETC,
+                                                                      SupportedDatasets.ECOSET100, SupportedDatasets.CLICKME]):#, SupportedDatasets.IMAGENET100]:
             num_train_shards = min(params.max_num_train, cfg.min_train_class_counts)
             num_val_shards = cfg.max_val_counts
             num_test_shards = params.max_num_test if params.max_num_test < np.inf else None
@@ -261,12 +280,12 @@ class ImageDatasetFactory(AbstractDatasetFactory):
             else:
                 test_dataset = ImageFolderWithAnnotations(os.path.join(params.datafolder, 'Annotations', 'val'), os.path.join(params.datafolder, 'val'), transform=test_transform)
         elif cfg.dataset_class == FixationPointDataset:
-            train_dataset = FixationPointDataset(params.datafolder, 'train', os.path.join(params.datafolder, 'logit_maps'), transform=train_transform)
+            train_dataset = FixationPointDataset(params.datafolder, 'train', transform=train_transform, **(params.kwargs))
             if os.path.exists(os.path.join(params.datafolder, 'test')):
-                test_dataset = FixationPointDataset(params.datafolder, 'test', os.path.join(params.datafolder, 'logit_maps'), transform=test_transform)
-                val_dataset = FixationPointDataset(params.datafolder, 'val', os.path.join(params.datafolder, 'logit_maps'), transform=test_transform)
+                test_dataset = FixationPointDataset(params.datafolder, 'test', transform=test_transform, **(params.kwargs))
+                val_dataset = FixationPointDataset(params.datafolder, 'val', transform=test_transform, **(params.kwargs))
             else:
-                test_dataset = FixationPointDataset(params.datafolder, 'val', os.path.join(params.datafolder, 'logit_maps'), transform=test_transform)
+                test_dataset = FixationPointDataset(params.datafolder, 'val', transform=test_transform, **(params.kwargs))
         elif params.dataset == SupportedDatasets.SPEECHCOMMANDS:
             train_dataset = SpeechCommandDatasetWrapper(params.datafolder, subset='training', download=True)
             val_dataset = SpeechCommandDatasetWrapper(params.datafolder, subset='validation', download=True)
@@ -282,17 +301,24 @@ class ImageDatasetFactory(AbstractDatasetFactory):
             train_dataset = SpeechCommandDatasetWrapper(params.datafolder, subset='training', download=True)
             val_dataset = SpeechCommandDatasetWrapper(params.datafolder, subset='validation', download=True)
             test_dataset = SpeechCommandDatasetWrapper(params.datafolder, subset='testing', download=True)
+        if params.dataset != SupportedDatasets.LIBRISPEECH:
+            filter_dataset_by_target(train_dataset, params.class_idxs)
+            train_idxs, val_idxs = make_val_dataset(train_dataset, nclasses, train_class_counts, class_idxs=params.class_idxs)
+            if len(val_idxs) > max_val_counts:
+                val_idxs = np.random.choice(val_idxs, max_val_counts, replace=False)
+            if 'val_dataset' not in locals():
+                val_dataset = torch.utils.data.Subset(train_dataset, val_idxs)
+            train_dataset = torch.utils.data.Subset(train_dataset, train_idxs)
 
-        filter_dataset_by_target(train_dataset, params.class_idxs)
-        train_idxs, val_idxs = make_val_dataset(train_dataset, nclasses, train_class_counts, class_idxs=params.class_idxs)
-        if len(val_idxs) > max_val_counts:
-            val_idxs = np.random.choice(val_idxs, max_val_counts, replace=False)
-        if 'val_dataset' not in locals():
-            val_dataset = torch.utils.data.Subset(train_dataset, val_idxs)
-        train_dataset = torch.utils.data.Subset(train_dataset, train_idxs)
-
-        filter_dataset_by_target(test_dataset, params.class_idxs)
-        test_idxs, _ = make_val_dataset(test_dataset, nclasses, test_class_counts, class_idxs=params.class_idxs)
-        test_dataset = torch.utils.data.Subset(test_dataset, test_idxs)
+            filter_dataset_by_target(test_dataset, params.class_idxs)
+            test_idxs, _ = make_val_dataset(test_dataset, nclasses, test_class_counts, class_idxs=params.class_idxs)
+            test_dataset = torch.utils.data.Subset(test_dataset, test_idxs)
+        else:
+            train_len = len(train_dataset)
+            if params.max_num_train < train_len:
+                train_dataset, _ = torch.utils.data.random_split(train_dataset, [params.max_num_train, train_len - params.max_num_train], torch.Generator().manual_seed(42))
+            test_len = len(test_dataset)
+            if params.max_num_test < test_len:
+                test_dataset, _ = torch.utils.data.random_split(test_dataset, [params.max_num_test, test_len - params.max_num_test], torch.Generator().manual_seed(42))
         print(f'train_dataset_len: {len(train_dataset)}, val_dataset_len: {len(val_dataset)}, test_dataset_len: {len(test_dataset)}')
         return train_dataset, val_dataset, test_dataset, nclasses
