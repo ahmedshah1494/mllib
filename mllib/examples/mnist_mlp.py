ac03d85c8b5c5a49f93fd502686d001ead1ade7c
ahmed <ahmedshah1494@gmail.com>
2023-11-02 17:02:09 -0400

    moved example task to mnist_mlp

--- mllib/examples/mnist_mlp.py
+++ mllib/examples/mnist_mlp.py
@@ -1,6 +1,40 @@
 from mllib.runners.base_runners import BaseRunner
-from mllib.tasks.base_tasks import MNISTMLP
+from mllib.tasks.base_tasks import AbstractTask
+from mllib.param import BaseParameters
 
+from mllib.runners.configs import BaseExperimentConfig, TrainingParams
+from mllib.trainers.base_trainers import PytorchLightningTrainer
+from mllib.datasets.dataset_factory import ImageDatasetFactory, SupportedDatasets
+from mllib.models.base_models import MLPClassifier
+from mllib.optimizers.configs import AdamOptimizerConfig, OneCycleLRConfig
+
+class MNISTMLP(AbstractTask):
+    def get_dataset_params(self) -> BaseParameters:
+        p = ImageDatasetFactory.get_params()
+        p.dataset = SupportedDatasets.MNIST
+        p.datafolder = '.'
+        p.max_num_train = 1000
+        p.max_num_test = 1000
+        return p
+    
+    def get_model_params(self) -> BaseParameters:
+        p = MLPClassifier.get_params()
+        p.widths = [32, 64]
+        p.input_size = 28*28
+        p.output_size = 10
+        return p
+    
+    def get_experiment_params(self) -> BaseExperimentConfig:
+        return BaseExperimentConfig(
+            PytorchLightningTrainer.TrainerParams(PytorchLightningTrainer,
+                TrainingParams(logdir='test-logs/', nepochs=1, early_stop_patience=50, tracked_metric='val_accuracy',
+                    tracking_mode='max', scheduler_step_after_epoch=False
+                )
+            ),
+            AdamOptimizerConfig(lr=0.2, weight_decay=5e-4),
+            OneCycleLRConfig(max_lr=0.001, epochs=1, steps_per_epoch=375, pct_start=0.1, anneal_strategy='linear'),
+            logdir='test-logs/', batch_size=128
+        )
 
 task = MNISTMLP()
 runner = BaseRunner(task)
--- mllib/tasks/base_tasks.py
+++ mllib/tasks/base_tasks.py
@@ -3,9 +3,6 @@ from typing import Callable
 import numpy as np
 import torch
 import types
-from mllib.datasets.dataset_factory import ImageDatasetFactory, SupportedDatasets
-from mllib.models.base_models import MLPClassifier
-from mllib.optimizers.configs import AdamOptimizerConfig, ReduceLROnPlateauConfig
 from mllib.param import BaseParameters
 from mllib.runners.configs import BaseExperimentConfig
 from attrs import define
@@ -72,28 +69,3 @@ class AbstractTaskV2:
         t.get_model_params = types.MethodType(lambda self: task_dict['model_params'], t)
         t.get_experiment_params = types.MethodType(lambda self: task_dict['experiment_params'], t)
         return t
-
-class MNISTMLP(AbstractTask):
-    def get_dataset_params(self) -> BaseParameters:
-        p = ImageDatasetFactory.get_params()
-        p.dataset = SupportedDatasets.MNIST
-        p.datafolder = '.'
-        p.max_num_train = 1000
-        p.max_num_test = 1000
-        return p
-    
-    def get_model_params(self) -> BaseParameters:
-        p = MLPClassifier.get_params()
-        p.widths = [32, 64]
-        p.input_size = 28*28
-        p.output_size = 10
-        return p
-    
-    def get_experiment_params(self) -> BaseExperimentConfig:
-        p = BaseExperimentConfig()
-        p.batch_size = 128
-        p.optimizer_config = AdamOptimizerConfig()
-        p.scheduler_config = ReduceLROnPlateauConfig()
-        p.training_params.nepochs = 2
-        p.logdir = 'logs'
-        return p
